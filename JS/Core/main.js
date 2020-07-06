f825978eaea1192b6fdc935c788721408adf7688
Zoxium <31391261+Zoxium@users.noreply.github.com>
2020-07-06 03:52:37 -0400

    Added stuff in main.js

--- JS/Core/main.js
+++ JS/Core/main.js
@@ -1,1 +1,11 @@
+var player = {
+    expontAmount: new OmegaNum(1),
+    expontMultperSec: new OmegaNum(1.00),
+}
 
+function update() {
+    document.getElementById("expontAmount").innerHTML = player.expontAmount
+    document.getElementById("expontMultperSec").innerHTML = player.expontMultperSec.toFixed(2)
+}
+
+setInterval(update, (1/15 * 1000))
